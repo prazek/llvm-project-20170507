99c2ff29733dbb81c718c9b2c1d2bce12fa6dca6
Piotr Padlewski <piotr.padlewski@gmail.com>
2018-05-23 22:52:24 +0200

    Simplify recursive launder.invariant.group and strip
    Summary:
    This patch is crucial for proving equality laundered/stripped
    pointers. eg:
    
      bool foo(A *a) {
        return a == std::launder(a);
      }
    
    Clang with -fstrict-vtable-pointers will emit something like:
    
        define dso_local zeroext i1 @_Z3fooP1A(%struct.A* %a) {
        entry:
          %c = bitcast %struct.A* %a to i8*
          %call = tail call i8* @llvm.launder.invariant.group.p0i8(i8* %c)
          %0 = bitcast %struct.A* %a to i8*
          %1 = tail call i8* @llvm.strip.invariant.group.p0i8(i8* %0)
          %2 = tail call i8* @llvm.strip.invariant.group.p0i8(i8* %call)
          %cmp = icmp eq i8* %1, %2
          ret i1 %cmp
        }
    
    and because %2 can be replaced with @llvm.strip.invariant.group(%0)
    and that %2 and %1 will produce the same value (because strip is readnone)
    we can replace compare with true.
    
    Reviewers: rsmith, hfinkel, majnemer, amharc, kuhar
    
    Subscribers: llvm-commits, hiraditya
    
    Differential Revision: https://reviews.llvm.org/D47423

--- llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCalls.cpp
@@ -1167,6 +1167,38 @@ static Instruction *simplifyMaskedGather(IntrinsicInst &II, InstCombiner &IC) {
   return nullptr;
 }
 
+/// This function transforms launder.invariant.group and strip.invariant.group
+/// like:
+/// launder(launder(%x)) -> launder(%x)       (the result is not the argument)
+/// launder(strip(%x)) -> launder(%x)
+/// strip(strip(%x)) -> strip(%x)             (the result is not the argument)
+/// strip(launder(%x)) -> strip(%x)
+/// This is legal because it preserves the most recent information about
+/// the presence or absence of invariant.group.
+static Instruction *simplifyInvariantGroupIntrinsic(IntrinsicInst &II,
+                                                    InstCombiner &IC) {
+  auto *Arg = II.getArgOperand(0);
+  auto *StrippedArg = Arg->stripPointerCasts();
+  auto *StrippedInvariantGroupsArg = Arg->stripPointerCastsAndInvariantGroups();
+  if (StrippedArg == StrippedInvariantGroupsArg)
+    return nullptr; // No launders/strips to remove.
+
+  Value *Result = nullptr;
+
+  if (II.getIntrinsicID() == Intrinsic::launder_invariant_group)
+    Result = IC.Builder.CreateLaunderInvariantGroup(StrippedInvariantGroupsArg);
+  else if (II.getIntrinsicID() == Intrinsic::strip_invariant_group)
+    Result = IC.Builder.CreateStripInvariantGroup(StrippedInvariantGroupsArg);
+  else
+    llvm_unreachable(
+        "simplifyInvariantGroupIntrinsic only handles launder and strip");
+
+  if (Result->getType() != II.getType())
+    Result = IC.Builder.CreateBitCast(Result, II.getType());
+
+  return cast<Instruction>(Result);
+}
+
 static Instruction *simplifyMaskedScatter(IntrinsicInst &II, InstCombiner &IC) {
   // If the mask is all zeros, a scatter does nothing.
   auto *ConstMask = dyn_cast<Constant>(II.getArgOperand(3));
@@ -1841,7 +1873,11 @@ Instruction *InstCombiner::visitCallInst(CallInst &CI) {
     return simplifyMaskedGather(*II, *this);
   case Intrinsic::masked_scatter:
     return simplifyMaskedScatter(*II, *this);
-
+  case Intrinsic::launder_invariant_group:
+  case Intrinsic::strip_invariant_group:
+    if (auto *SkippedBarrier = simplifyInvariantGroupIntrinsic(*II, *this))
+      return replaceInstUsesWith(*II, SkippedBarrier);
+    break;
   case Intrinsic::powi:
     if (ConstantInt *Power = dyn_cast<ConstantInt>(II->getArgOperand(1))) {
       // 0 and 1 are handled in instsimplify
